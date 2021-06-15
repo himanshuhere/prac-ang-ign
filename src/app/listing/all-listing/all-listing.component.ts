196e969387c68cb58f8b9dae82344cfcc4303c93
Himanshu Soni <himanshusoni3105@outlook.com>
2021-06-15 14:32:18 +0530

    fist

---
+++ src/app/listing/all-listing/all-listing.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-all-listing',
+  templateUrl: './all-listing.component.html',
+  styleUrls: ['./all-listing.component.css']
+})
+export class AllListingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
